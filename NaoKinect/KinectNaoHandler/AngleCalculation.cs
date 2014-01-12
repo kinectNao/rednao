a0c21782c79b1fc25613e63dacf3a8384a76042a
Mitsch <Mitsch@W7_Home>
2014-01-12 17:30:37 +0100

    Alle 4 Erkennugnsmethoden implementiert TODO: Test TODO: Umrechnung auf Nao Raum (einfache addition/subtraktion mit 180° bzw. 90°) TODO: Programm beendet sich nicht richtig beim Debuggen

--- NaoKinect/KinectNaoHandler/AngleCalculation.cs
+++ NaoKinect/KinectNaoHandler/AngleCalculation.cs
@@ -13,12 +13,38 @@ namespace KinectNaoHandler
 
         public static float getShoulderPitch_Right(Skeleton skeleton)
         {
-            return -999.0f;
+            // Get fitting joints
+            Joint hip_j = skeleton.Joints[JointType.HipRight];
+            Joint shoulder_j = skeleton.Joints[JointType.ShoulderRight];
+            Joint elbow_j = skeleton.Joints[JointType.ElbowRight];
+           
+
+            //Vector of Joints
+            Vector3D hip = new Vector3D(hip_j.Position.X, hip_j.Position.Y, hip_j.Position.Z);
+            Vector3D shoulder = new Vector3D(shoulder_j.Position.X, shoulder_j.Position.Y, shoulder_j.Position.Z);
+            Vector3D elbow = new Vector3D(elbow_j.Position.X, elbow_j.Position.Y, elbow_j.Position.Z);
+            
+
+            // Vektorberechnung
+            return AngleCalculation.getAngle(hip, shoulder, elbow);
         }
 
         public static float getShoulderRoll_Right(Skeleton skeleton)
         {
-            return -999.0f;
+            // Get fitting joints
+            Joint hip_right_j = skeleton.Joints[JointType.HipRight];
+            Joint hip__left_j = skeleton.Joints[JointType.HipLeft];
+            Joint shoulder_j = skeleton.Joints[JointType.ShoulderRight];
+            Joint elbow_j = skeleton.Joints[JointType.ElbowRight];
+
+            //Vector of Joints
+            Vector3D hip_right = new Vector3D(hip_right_j.Position.X, hip_right_j.Position.Y, hip_right_j.Position.Z);
+            Vector3D hip_left = new Vector3D(hip__left_j.Position.X, hip__left_j.Position.Y, hip__left_j.Position.Z);
+            Vector3D shoulder = new Vector3D(shoulder_j.Position.X, shoulder_j.Position.Y, shoulder_j.Position.Z);
+            Vector3D elbow = new Vector3D(elbow_j.Position.X, elbow_j.Position.Y, elbow_j.Position.Z);
+            
+            // Vektorberechnung
+            return AngleCalculation.getAngle(hip_right, hip_left, shoulder, elbow);
         }
 
         public static float getElbowRoll_Right(Skeleton skeleton)
