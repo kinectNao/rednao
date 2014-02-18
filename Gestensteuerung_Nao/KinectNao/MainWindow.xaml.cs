e3094fccaa51c867001bdfefbec24c4dbb7c266e
rednao_dhbw <rednao@dhbw>
2014-02-18 15:15:26 +0100

    Kinect Frame wie vorher (rote Punkte)

--- Gestensteuerung_Nao/KinectNao/MainWindow.xaml.cs
+++ Gestensteuerung_Nao/KinectNao/MainWindow.xaml.cs
@@ -23,14 +23,14 @@ namespace KinectNao
     /// </summary>
     public partial class MainWindow : Window
     {
-        public delegate void UserSkeleton();
+        public delegate void PixelData();
+        public delegate void SkeletonData();
 
         KinectSensor mySensor;
         WriteableBitmap myBitmap;
-        byte[] myColorArray;
-        Skeleton[] mySkeletonArray;
-
-
+        byte[] myPixelData;
+    
+        Skeleton[] skeletons;
 
         public MainWindow()
         {
@@ -59,14 +59,13 @@ namespace KinectNao
                 mySensor.SkeletonStream.Enable();
 
 
-                myColorArray = new byte[this.mySensor.ColorStream.FramePixelDataLength];
+                myPixelData = new byte[this.mySensor.ColorStream.FramePixelDataLength];
                 myBitmap = new WriteableBitmap(this.mySensor.ColorStream.FrameWidth, this.mySensor.ColorStream.FrameHeight, 96.0, 96.0, PixelFormats.Bgr32, null);
-                mySkeletonArray = new Skeleton[this.mySensor.SkeletonStream.FrameSkeletonArrayLength];
-
                 videoImage.Source = myBitmap;
                 
                 //Event für Videobild registrieren
-                mySensor.AllFramesReady += new EventHandler<AllFramesReadyEventArgs>(mySensorAllFramesReady);
+                mySensor.ColorFrameReady += this.runtime_VideoFrameReady;
+                mySensor.SkeletonFrameReady += this.runtime_SkeletonFrameReady;
                
                 try
                 {
@@ -79,45 +78,7 @@ namespace KinectNao
             }
         }
 
-        private void mySensorAllFramesReady(object sender, AllFramesReadyEventArgs e)
-        {
-            ColorImageFrame c = e.OpenColorImageFrame();
-           SkeletonFrame s = e.OpenSkeletonFrame();
-
-           
-
-           
-
-                        if (null == c || null == s) return;
-
-                        c.CopyPixelDataTo(myColorArray);
-                        s.CopySkeletonDataTo(mySkeletonArray);
-
-                        BitmapSource bs = BitmapSource.Create(640, 480, 96, 96, PixelFormats.Bgr32, null, myColorArray, 640 * 4);
-                        DrawingVisual drawingVisual = new DrawingVisual();
-                        DrawingContext drawingContent = drawingVisual.RenderOpen();
-                        drawingContent.DrawImage(bs, new Rect(0, 0, 640, 480));
-
-                        //Knochen zeichnen
-
-                        drawingContent.Close();
-                        RenderTargetBitmap myTarget = new RenderTargetBitmap(640, 480, 96, 96, PixelFormats.Pbgra32);
-                        myTarget.Render(drawingVisual);
-           
-                         if (null != Application.Current)
-                                {
-                                    Application.Current.Dispatcher.BeginInvoke((UserSkeleton)delegate
-                                    {
-                                        videoImage.Source = myTarget;
-                                    });
-                                 }
-           
-              
-        }
-
         
-
-
         //Fenster geschlossen
         void MainWindow_Unloaded(object sender, RoutedEventArgs e)
         {
@@ -126,6 +87,113 @@ namespace KinectNao
                 mySensor.Stop();
             }
         }
+
+ 
+        void runtime_VideoFrameReady(object sender, ColorImageFrameReadyEventArgs e)
+        {
+            using (ColorImageFrame colorFrame = e.OpenColorImageFrame())
+            {
+                if (null != colorFrame)
+                {
+                    colorFrame.CopyPixelDataTo(myPixelData);
+                    if (null != Application.Current)
+                    {
+                         Application.Current.Dispatcher.BeginInvoke((PixelData)delegate
+                         {
+                             myBitmap.WritePixels(new Int32Rect(0, 0, myBitmap.PixelWidth, myBitmap.PixelHeight), myPixelData, myBitmap.PixelWidth * sizeof(int), 0);
+                         });
+                    }
+                   
+                   
+                }
+            }
+        }
+
+
+
+        void runtime_SkeletonFrameReady(object sender, SkeletonFrameReadyEventArgs e)
+        {
+            bool receivedData = false;
+
+            using (SkeletonFrame SFrame = e.OpenSkeletonFrame())
+            {
+                if (SFrame == null)
+                {
+                    // The image processing took too long. More than 2 frames behind.
+                }
+                else
+                {
+                    skeletons = new Skeleton[SFrame.SkeletonArrayLength];
+                    SFrame.CopySkeletonDataTo(skeletons);
+                    receivedData = true;
+                }
+            }
+
+            if (receivedData)
+            {
+
+                Skeleton currentSkeleton = (from s in skeletons
+                                            where s.TrackingState == SkeletonTrackingState.Tracked
+                                            select s).FirstOrDefault();
+
+                if (currentSkeleton != null)
+                {
+                    //Berechnungen einfügen
+                    //anglehandler.updateSkeleton(currentSkeleton);
+                    if (null != Application.Current)
+                    {
+                        Application.Current.Dispatcher.BeginInvoke((PixelData)delegate
+                        {
+                            SetEllipsePosition(head, currentSkeleton.Joints[JointType.Head]);
+                            SetEllipsePosition(leftHand, currentSkeleton.Joints[JointType.HandLeft]);
+                            SetEllipsePosition(rightHand, currentSkeleton.Joints[JointType.HandRight]);
+                        });
+                    }
+                }
+            }
+        }
+
+
+
+        //This method is used to position the ellipses on the canvas
+        //according to correct movements of the tracked joints.
+
+        //IMPORTANT NOTE: Code for vector scaling was imported from the Coding4Fun Kinect Toolkit
+        //available here: http://c4fkinect.codeplex.com/
+        //I only used this part to avoid adding an extra reference.
+        private void SetEllipsePosition(Ellipse ellipse, Joint joint)
+        {
+            Microsoft.Kinect.SkeletonPoint vector = new Microsoft.Kinect.SkeletonPoint();
+            vector.X = ScaleVector(640, joint.Position.X);
+            vector.Y = ScaleVector(480, -joint.Position.Y);
+            vector.Z = joint.Position.Z;
+
+            Joint updatedJoint = new Joint();
+            updatedJoint = joint;
+            updatedJoint.TrackingState = JointTrackingState.Tracked;
+            updatedJoint.Position = vector;
+
+
+            Canvas.SetLeft(ellipse, updatedJoint.Position.X);
+            Canvas.SetTop(ellipse, updatedJoint.Position.Y);
+
+
+        }
+
+        private float ScaleVector(int length, float position)
+        {
+            float value = (((((float)length) / 1f) / 2f) * position) + (length / 2);
+            if (value > length)
+            {
+                return (float)length;
+            }
+            if (value < 0f)
+            {
+                return 0f;
+            }
+            return value;
+        }
+
 
 
 
