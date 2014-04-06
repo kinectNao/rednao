d60e1006c7ef464ffe1a2a827dae32335b8b0270
Mitsch <Michael@home>
2014-04-06 15:41:43 +0200

    Kinect SDK-Kaptiel erweitert Grundstruktur für Architekturkapitel notiert

---
+++ Dokumentation/Listings/Kinect_Skeleton.cs
@@ -0,0 +1,26 @@
+using (SkeletonFrame sFrame = e.OpenSkeletonFrame())
+{
+	if (sFrame  == null)
+	{
+          // The image processing took too long. More than 2 frames behind.
+	  return;
+        }
+        else
+        {
+		skeletons = new Skeleton[sFrame.SkeletonArrayLength];
+                sFrame.CopySkeletonDataTo(skeletons);
+                receivedData = true;
+        }
+        if (receivedData)
+        {
+		Skeleton currentSkeleton = (	from s in skeletons
+                                                where s.TrackingState == SkeletonTrackingState.Tracked
+                                                select s).FirstOrDefault();
+
+	        if (currentSkeleton != null)
+                {
+			//Berechnungen einfügen
+                        anglehandler.updateSkeleton(currentSkeleton);
+                }
+        }
+}
