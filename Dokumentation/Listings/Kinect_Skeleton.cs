9d22b112e0ada2308d16005906709662616cc62d
Mitsch <Michael@home>
2014-04-07 22:16:20 +0200

    Kinect SDK und Software Kapitel erweitert

--- Dokumentation/Listings/Kinect_Skeleton.cs
+++ Dokumentation/Listings/Kinect_Skeleton.cs
@@ -1,26 +1,27 @@
 using (SkeletonFrame sFrame = e.OpenSkeletonFrame())
 {
-	if (sFrame  == null)
-	{
-          // The image processing took too long. More than 2 frames behind.
-	  return;
-        }
-        else
-        {
+    if (sFrame  == null)
+    {
+         // The image processing took too long. More than 2 frames behind.
+	 return;
+    }
+    else
+    {
 		skeletons = new Skeleton[sFrame.SkeletonArrayLength];
                 sFrame.CopySkeletonDataTo(skeletons);
                 receivedData = true;
-        }
-        if (receivedData)
-        {
-		Skeleton currentSkeleton = (	from s in skeletons
-                                                where s.TrackingState == SkeletonTrackingState.Tracked
-                                                select s).FirstOrDefault();
+    }
+    if (receivedData)
+    {
+		Skeleton currentSkeleton =
+		 ( from s in skeletons
+                   where s.TrackingState == SkeletonTrackingState.Tracked
+                   select s).FirstOrDefault();
 
-	        if (currentSkeleton != null)
-                {
-			//Berechnungen einfügen
-                        anglehandler.updateSkeleton(currentSkeleton);
-                }
+  	if (currentSkeleton != null)
+        {
+           //Handler mit neuen Skelettdaten versorgen
+           anglehandler.updateSkeleton(currentSkeleton);
         }
+    }
 }
